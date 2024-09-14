19abf501b7d4d6c7149531c1e7ee4b8f1542346f
Field-Effect-Transistor <145497915+Field-Effect-Transistor@users.noreply.github.com>
2024-09-14 04:56:52 +0300

    Lab1

---
+++ Lab1/Lab1ShapovalCE306.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+/**
+* Клас реалізує приклад програи за вимогами лабораторної роботи 1
+*/
+public class Lab1ShapovalCE306 {
+    /**
+     * Статичний метод main є точкою входу в програму
+     * @param args
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        int rows;
+        char[][] matrix;
+    }
+}

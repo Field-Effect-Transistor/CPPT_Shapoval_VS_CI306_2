077352b9cdc365c055114804d551b2af308e4347
Field-Effect-Transistor <145497915+Field-Effect-Transistor@users.noreply.github.com>
2024-10-14 22:20:45 +0300

    merge squash lab4

---
+++ Lab4/EquationApp.java
@@ -0,0 +1,28 @@
+import ce306.shapoval.lab4.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class EquationApp {
+    public static void main(String[] args) {
+        try {
+            System.out.print("Enter file name:\t");
+            Scanner in = new Scanner(System.in);
+            PrintWriter fout = new PrintWriter(new File(in.nextLine()));
+            try {
+                System.out.print("Enter x in degrees:\t");
+                //double result = new Equation().calculate(in.nextDouble());
+                fout.println(new Equation().calculate(in.nextDouble()));
+                System.out.println("Result saved to file.");
+            } catch (CalcException calcException) {
+                System.out.println(calcException.getMessage());
+            } finally {
+                fout.flush();
+                fout.close();
+            }
+        } catch (FileNotFoundException fileException) {
+            System.out.print("Exception reason: perhaps wrong file path");
+        }
+    }
+}
